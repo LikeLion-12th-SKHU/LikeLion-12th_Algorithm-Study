8110cb763def7ce9588837e784122db6401ec375
shinheekim <shinhee1241@gmail.com>
2024-11-10 17:15:24 +0900

    [BOJ] 13669 점화식 / 실버 4 / 40분

---
+++ shinhee/BOJ/src/java_13699/Main.java
@@ -0,0 +1,26 @@
+package BOJ.src.java_13699;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n + 1];
+
+        dp[0] = 1;
+        if (n >= 1) {
+            dp[1] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
