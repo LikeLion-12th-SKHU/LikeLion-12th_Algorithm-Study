fae7eec31014071419da63b5097b70a7c81a1c29
djdongjae <jk47wee@gmail.com>
2024-11-11 20:36:55 +0900

    [BOJ] LCS / 골드 5 / 30분

---
+++ dongjae/BOJ/src/java_9251/Main.java
@@ -0,0 +1,32 @@
+package java_9251;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static char[] str1, str2;
+    public static int m, n;
+    public static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str1 = br.readLine().toCharArray();
+        str2 = br.readLine().toCharArray();
+
+        m = str1.length;
+        n = str2.length;
+
+        dp = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1[i-1] == str2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        System.out.println(dp[m][n]);
+    }
+}
