8294b16d332f17da75647c34916c824f32033094
shinheekim <shinhee1241@gmail.com>
2024-11-19 01:21:10 +0900

    [BOJ] 15649 N과 M(1) / 실버 3 / 50분

---
+++ shinhee/BOJ/src/java_15649/Main.java
@@ -0,0 +1,50 @@
+package BOJ.src.java_15649;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        backTracking(0);
+        System.out.println(sb);
+    }
+
+    public static void backTracking(int depth){
+        if (depth == M){
+            for(int val : arr){
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                NMBackTracking(depth + 1);
+
+                visit[i] = false;
+            }
+        }
+    }
+}
